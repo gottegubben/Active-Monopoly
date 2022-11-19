327a5cdfe63d713308c5afd7059b243c13785de5
gottegubben <lukas.soderberg0404@gmail.com>
2022-11-19 20:41:34 +0100

    Commit 2

---
+++ Project/Classes/Tiles/PurchasableTile.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Monopoly
+{
+    abstract public class PurchasableTile : Tile
+    {
+        /// <summary>
+        /// This class will represent all the purchasable tiles in the game of Monopoly.
+        /// </summary>
+
+        #region Properties:
+        //The index of the group correlated to the tile. For instance can there be 3 streets and they all have the same group index to indicate that they are correlated.
+        public int GroupIndex { get; }
+
+        //The player that owns the property.
+        public Player Owner { get; private set; }
+        #endregion
+
+        #region Methods:
+        //Changes the current owner of the tile to a new owner.
+        public void ChangeOwner(Player newOwner)
+        {
+            Owner = newOwner;
+        }
+
+        public virtual void GetRent(int houses)
+        {
+
+        }
+        #endregion
+    }
+}
---
+++ Project/Classes/Tiles/Tile.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Monopoly
+{
+    public abstract class Tile
+    {
+        /// <summary>
+        /// The tile class will represent a tile in the game of monopoly. For instance: the "GO - tile". This will be the base class for all tiles.
+        /// </summary>
+
+        #region Properties: 
+        //The name of the tile.
+        public string TileName { get; }
+
+        //The id of the tile.
+        public string TileId { get; }
+
+        //The color of the tile. For instance the color of the street.
+        public Color TileColor { get; }
+        #endregion
+    }
+}
